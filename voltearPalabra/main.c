46f263de94db00f50b6fcfe2344be9fc11d17740
AlvarOnce <alvaropzlaestrella@gmail.com>
2026-02-16 20:03:23 +0100

    voltear palabra

---
+++ voltearPalabra/main.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char palabra[100];
+    int i, j;
+    char temp;
+
+    printf("Introduce una palabra: ");
+    if (scanf("%s", palabra) != 1) return 1;
+
+    size_t n = strlen(palabra);
+
+    for (i = 0, j = (int)n - 1; i < j; i++, j--) {
+        temp = palabra[i];
+        palabra[i] = palabra[j];
+        palabra[j] = temp;
+    }
+
+    printf("Palabra invertida: %s\n", palabra);
+
+    return 0;
+}
