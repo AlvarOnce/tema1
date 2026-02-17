1da41d8cdd5b71a54571a56f4f76159dd7f661db
AlvarOnce <alvaropzlaestrella@gmail.com>
2026-02-17 10:59:02 +0100

    cambios en freeMemoria

--- freeMemoria/main.c
+++ freeMemoria/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define TAM 3
 
 int main(void) {
-	int* p = NULL;
-	free(p);
+
+	int lista_pila[TAM] = { 0,1,2 };
+
+	int* lista_heap = malloc(TAM*sizeof(*lista_heap));
+	if (lista_heap == NULL) return 1;
+	if (lista_heap == NULL) return 1
+
+	free(*lista_heap);
+	lista_heap = NULL;
+
 	return 0;
 }
